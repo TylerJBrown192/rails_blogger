ec1852fef0355e957ecfff49fd964325b74ae836
Tyler Brown <elementalplug@gmail.com>
2015-03-20 10:40:27 -0700

    Build out initial file structure and add edit functionality to first table

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:id])
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+end
