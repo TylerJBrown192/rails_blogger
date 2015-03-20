803a237bb8344301f005c6d1a6a6bbcec972c3ae
Tyler Brown <elementalplug@gmail.com>
2015-03-20 16:59:10 -0700

    Fixed bug with edit functionality on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,8 +16,33 @@ class CommentsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    if @comment.update(comment_params)
+      flash[:notice] = "Comment successfully updated!"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    flash[:notice] = "Comment deleted."
+    redirect_to post_path(@post)
+  end
+
 private
   def comment_params
-    params.require(:comment).permit(:user_comment, :post_id)
+    params.require(:comment).permit(:user_comment)
   end
+
 end
